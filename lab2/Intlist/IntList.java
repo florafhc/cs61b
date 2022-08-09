52e97edc2bd1b805449d9737b2dbdd987ba54551
Haichen Fu <haichenfu@HaichendeMacBook-Pro.local>
2022-08-09 23:47:26 +0800

    lab2 updated

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,13 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null){
+            A = B;
+            return A;
+        }else {
+            A.rest = dcatenate(A.rest, B);
+            return A;
+        }
     }
 
     /**
@@ -90,8 +95,13 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null){
+            return B;
+        }else {
+            IntList rest = catenate(A.rest, B);
+            IntList M = new IntList(A.first, rest);
+            return M;
+        }
     }
 
 
